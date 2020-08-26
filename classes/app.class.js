72f1be3ce8b1e4fe8e3952097141302f24e2b5c7
Archizavr <47066517+Archizavr@users.noreply.github.com>
2020-08-26 20:40:07 +0300

    4. commit

--- classes/app.class.js
+++ classes/app.class.js
@@ -5,10 +5,40 @@ class App {
     constructor() {
         this.canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
         this.generator = new Generator();
-    }
+        this.scoreboard = new Scoreboard();
 
-    run() {
-        this.canvas.clear();
-        this.generator.drawCoins(this.canvas.context);
+     /* Create interval  */
+        const self = this;
+      setInterval(function() {
+        self.run();
+        }, REFRESH_RATE);
+
+    
+
+    this.canvas.context.canvas.addEventListener("click", event => {
+        const clickX = event.x - event.target.getBoundingClientRect().left;
+        const clickY = event.y - event.target.getBoundingClientRect().right;
+
+        for (let i = 0; i < self.generator.coins.length; i++) {
+            if (clickX >= coin.x &&
+                clickX <= coin.x + coin.size &&
+                clickY >= coin.y &&
+                clickY <= coin.y + coin.size) {
+                    self.clickCoin(coin);
+                }
+            }
+        });
     }
+        run() {
+            this.generator.generate();
+
+            this.canvas.clear();
+            this.generator.drawCoins(this.canvas.context);
+            this.scoreboard.draw(this.canvas.context);
+        }
+
+        clickCoin(coin) {
+            this.scoreboard.score += coin.value;
+            this.generator.expireCoin(coin);
+        }
 }
--- classes/generator.class.js
+++ classes/generator.class.js
@@ -10,6 +10,10 @@ class Generator {
         this.coins.push(newCoin);
         newCoin.x = Math.floor( Math.random() * (1 + CANVAS_WIDTH - newCoin.size) );
         newCoin.y = Math.floor( Math.random() * (1 + CANVAS_HEIGHT - newCoin.size) );
+        const self = this;
+        setTimeout(function() {
+            self.expireCoin(newCoin)
+        }, COIN_LIFESPAN)
     }
 
     drawCoins(context) {
@@ -17,5 +21,15 @@ class Generator {
             this.coins[i].draw(context);
         }
     }
-
+    /**
+     * Removes a coin from the list of all coins.
+     * @param {Coin} coin Coin to remove
+     */
+    expireCoin(coin) {
+        const index = this.coins.indexOf(coin);
+        if(index > -1) {
+            this.coins.splice(index, 1);
+        }
+                 
+    }
 }
